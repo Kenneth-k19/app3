1f30173ec2fd52e166b4c09a14d078dd365c8627
Kenneth-k19 <136780026+Kenneth-k19@users.noreply.github.com>
2023-10-30 11:07:31 -0600

    update2

--- app3/lib/contactos_page.dart
+++ app3/lib/contactos_page.dart
@@ -11,8 +11,74 @@ class contactos extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Contactos'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: SizedBox(
+          width: double.infinity,
+          child:  Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                child: Text('P',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, 
+                      fontSize: 50
+                  ),
+                ),
+              ),
+        
+              const SizedBox(height: 20,),
+              const Text(
+                'Pascualillo',
+                style: TextStyle(fontSize: 25),
+                ),
+
+              const SizedBox(height: 15,),
+              Container(
+                height: 70,
+                decoration: const BoxDecoration(
+                  border: Border(
+                    top: BorderSide(color: Colors.black12),
+                    
+                    bottom: BorderSide(color: Colors.black12),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.only(top: 12),
+                  child:  SizedBox(
+                    width: double.infinity,
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        Column(
+                          children: [
+                            Icon(Icons.phone),
+                            Text('Llamar'),
+                          ],
+                        ),
+                        Spacer(),
+                  
+                        Column(
+                          children: [
+                            Icon(Icons.message),
+                            Text('Mensaje de texto'),
+                          ],
+                        ),
+                        Spacer(),
+                  
+                        Column(
+                          children: [
+                            Icon(Icons.videocam),
+                            Text('Video'),
+                          ],
+                        ),
+                        Spacer(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       );
   }
